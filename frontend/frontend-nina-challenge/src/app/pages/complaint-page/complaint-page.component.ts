0d5210a580173ce91d17ca9627061accbfcb5fe3
Olavo Conceicao <orc@cin.ufpe.br>
2023-03-12 01:38:52 -0300

    <component> complaint: carregar lista de complaint segundo cada tipo

--- frontend/frontend-nina-challenge/src/app/pages/complaint-page/complaint-page.component.ts
+++ frontend/frontend-nina-challenge/src/app/pages/complaint-page/complaint-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Complaints } from 'src/app/models/complaint/complaints.model';
+import { ComplaintsService } from 'src/app/services/complaints/complaints.service';
 
 @Component({
   selector: 'app-complaint-page',
@@ -7,8 +8,45 @@ import { Complaints } from 'src/app/models/complaint/complaints.model';
   styleUrls: ['./complaint-page.component.scss']
 })
 export class ComplaintPageComponent {
-  listaComplaints = [];
-  listaComplaintsUnsolved = [];
-  listaComplaintsInProgress = [];
-  listaComplaintsSolved = [];
+  listaComplaints: Complaints[] = [];
+  listaComplaintsUnsolved: Complaints[] = [];
+  listaComplaintsInProgress: Complaints[] = [];
+  listaComplaintsSolved: Complaints[] = [];
+
+  constructor(private complaintsService: ComplaintsService) {
+    
+  }
+
+  ngOnInit() {
+    this.getComplaints();
+  }
+
+  getComplaints() {
+    this.complaintsService.getComplaints().subscribe((response: Complaints[]) => {
+      response.forEach(complaint => {
+        this.listaComplaints.push(new Complaints(
+          complaint.id,
+          complaint.place,
+          complaint.at_moment,
+          complaint.datetime,
+          complaint.modified_at,
+          complaint.created_at,
+          complaint.description,
+          complaint.situation,
+          complaint.type[0]
+        ));
+      })
+     
+      this.listaComplaintsUnsolved = this.listaComplaints.filter(complaint => complaint.situation[0] === 'unsolved');
+      this.listaComplaintsInProgress = this.listaComplaints.filter(complaint => complaint.situation[0] === 'in progress');
+      this.listaComplaintsSolved = this.listaComplaints.filter(complaint => complaint.situation[0] === 'solved');
+      
+    }, error => {
+      console.log(error);
+    });
+  }
+
+
+
+
 }
