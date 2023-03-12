c73a4e4b0796adf3c424fb5a3e9456e639ec65bb
Olavo Conceicao <orc@cin.ufpe.br>
2023-03-11 23:17:22 -0300

    <page> criação da pagina e roteamento

--- frontend/frontend-nina-challenge/src/app/app-routing.module.ts
+++ frontend/frontend-nina-challenge/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComplaintPageComponent } from './pages/complaint-page/complaint-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component : ComplaintPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/frontend-nina-challenge/src/app/app.module.ts
+++ frontend/frontend-nina-challenge/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComplaintPageComponent } from './pages/complaint-page/complaint-page.component';
+import { CardHeaderComponent } from './components/card-header/card-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComplaintPageComponent,
+    CardHeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
