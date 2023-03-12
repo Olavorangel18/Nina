bb74770c6799009d7d27f92e77ede37ffe12242e
Olavo Conceicao <orc@cin.ufpe.br>
2023-03-11 23:39:36 -0300

    <component> criação da lista de complaints, junto com um pouco de sua estilização

---
+++ frontend/frontend-nina-challenge/src/app/components/list-complaints/list-complaints.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-complaints',
+  templateUrl: './list-complaints.component.html',
+  styleUrls: ['./list-complaints.component.scss']
+})
+export class ListComplaintsComponent {
+
+}
