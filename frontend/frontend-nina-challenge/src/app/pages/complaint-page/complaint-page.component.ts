25b29b0de7919626898cc6682da5a1e3377dcc3b
Olavo Conceicao <orc@cin.ufpe.br>
2023-03-11 23:17:41 -0300

    <page> complaint: pagina de complaints criada

---
+++ frontend/frontend-nina-challenge/src/app/pages/complaint-page/complaint-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-complaint-page',
+  templateUrl: './complaint-page.component.html',
+  styleUrls: ['./complaint-page.component.scss']
+})
+export class ComplaintPageComponent {
+
+}
