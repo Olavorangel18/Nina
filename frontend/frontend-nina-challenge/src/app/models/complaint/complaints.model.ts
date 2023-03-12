3db75079b3cb06f1d4edbd48149755bb7d633a48
Olavo Conceicao <orc@cin.ufpe.br>
2023-03-11 23:50:02 -0300

    <model> complaints: definição da classe com seus atributos

--- frontend/frontend-nina-challenge/src/app/models/complaint/complaints.model.ts
+++ frontend/frontend-nina-challenge/src/app/models/complaint/complaints.model.ts
@@ -1,2 +1,24 @@
 export class Complaints {
+    id:string
+    place:string
+    at_moment:string
+    datetime:string
+    modified_at:string
+    created_at:string
+    description:string
+    situation:string[]
+    type:string
+
+    constructor(id:string, place:string, at_moment:string, datetime:string, modified_at:string, created_at:string, description:string, situation:string[], type:string){
+        this.id = id
+        this.place = place
+        this.at_moment = at_moment
+        this.datetime = datetime
+        this.modified_at = modified_at
+        this.created_at = created_at
+        this.description = description
+        this.situation = situation
+        this.type = type
+    }
+    
 }
