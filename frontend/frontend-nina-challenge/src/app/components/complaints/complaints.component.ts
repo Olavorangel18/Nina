c3862be360dd74982331c9d3a236560b67b0658d
Olavo Conceicao <orc@cin.ufpe.br>
2023-03-12 01:00:55 -0300

    <component> finalização do componente de complaints

--- frontend/frontend-nina-challenge/src/app/components/complaints/complaints.component.ts
+++ frontend/frontend-nina-challenge/src/app/components/complaints/complaints.component.ts
@@ -8,20 +8,8 @@ import { Complaints } from 'src/app/models/complaint/complaints.model';
 })
 export class ComplaintsComponent {
   @Input() complaint: Complaints | undefined;
-  constructor() {
-    this.complaint = new Complaints(
-      'fd50bd52-b008-4c8a-9256-e4a6112538b0', 
-      'beach', 
-      false,
-       '2020-08-21T18:35:27.387Z', 
-       '2020-09-30T12:00:27.094Z', 
-       '2021-12-22T08:21:22.779Z', 
-       'Complaint 1', 
-       [
-        "unsolved"
-       ], 
-       'noise');
-   }
+  constructor() {}
+
 
   ngOnInit(): void {
   }
